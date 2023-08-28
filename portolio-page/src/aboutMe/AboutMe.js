421adb4acf3f5a5bb21d1ebd5edf95d5487f3178
Zach Robles <zrobles1188@gmail.com>
2023-08-28 16:38:10 -0400

    about me section, additional spacing between componenets and left hand text alignment

---
+++ portolio-page/src/aboutMe/AboutMe.js
@@ -0,0 +1,27 @@
+function AboutMe() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>
+        Im Zachary, but please feel free to call me Zach. Im always inspired by
+        learning new things and coding provides a limitless spring of new things
+        to learn. Solving problems has always provided a unique degree of
+        satisfaction, the the world of software development has given a level of
+        tangibility to complex problems that has incited a passion for coding in
+        me. Ive worked in customer service, ive worked in sales, Ive driven
+        forklifts and box trucks, Ive built patios and delivered Appliances, all
+        the while, one thing has been the same, Ive loved being part of a team,
+        and Ive loved reaching towards a common goal. I may be new to the world
+        of sofware development, but I am eager and excited to learn and grow as
+        a developer. When Im not coding I train Brazilian Jiu Jitsu and Rock
+        Climb. I play a number of musical instruments and am an avid painter, I
+        also enjoy table top games. I try to serve my community and currently
+        tutor local underserved spanish speaking 3rd-6th graders. I strive for a
+        greater good in all that I do and I hope to bring that to the table as a
+        developer.
+      </p>
+    </div>
+  );
+}
+
+export default AboutMe;
--- portolio-page/src/landingElements/Home.js
+++ portolio-page/src/landingElements/Home.js
@@ -1,16 +1,33 @@
 import Carosaul from "./Carosaul";
+import AboutMe from "../aboutMe/AboutMe";
 
 function Home() {
+  const extraSpace = {
+    height: "200px",
+  };
+
   return (
     <div className="container">
-      <div className="row align-items-center">
-        <h1>Zach Robles</h1>
-        <h2>Software Engineer</h2>
-        <h3>Full Stack Developer</h3>
+      <div className="row mt-4 mb-5">
+        <div className="col-md-8 mx-auto">
+          <h6>
+            <span className="text-muted">Hello, my name is</span>
+          </h6>
+          <h1 className="display-1">Zach Robles</h1>
+          <h2 className="display-3">Im a Full Stack Developer</h2>
+          <h3>Currently in the Greater DC Metropolotan area</h3>
+        </div>
       </div>
+      <div className="row align-items-center" style={extraSpace}></div>
       <div className="row align-items-center">
-        <div className="col-md-6 mx-auto">
+        <div className="col-md-8 mx-auto">
           <Carosaul />
+        </div>
+      </div>
+      <div className="row align-items-center" style={extraSpace}></div>
+      <div className="row align-items-center mt-4 mb-4">
+        <div className="col-md-6 mx-auto">
+          <AboutMe />
         </div>
       </div>
     </div>
