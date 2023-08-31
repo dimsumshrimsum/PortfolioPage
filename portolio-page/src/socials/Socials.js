8fa18658154cae493d5b391f10e32a7355533a68
Zach Robles <zrobles1188@gmail.com>
2023-08-31 17:40:40 -0400

    updated icon styling

--- portolio-page/src/socials/Socials.js
+++ portolio-page/src/socials/Socials.js
@@ -4,20 +4,28 @@ function Socials() {
   return (
     <div className="socials-container">
       <div className="socials">
-        <a href="https://www.linkedin.com/in/zacharyrobles/">
-          <i class="bi bi-linkedin"></i>
+        <a
+          href="https://www.linkedin.com/in/zacharyrobles/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <i class="bi bi-linkedin social-icon"></i>
         </a>
-        <a href="https://github.com/dimsumshrimsum">
-          <i class="bi bi-github"></i>
+        <a
+          href="https://github.com/dimsumshrimsum"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <i class="bi bi-github social-icon"></i>
         </a>
         <a href="mailto:zrobles1188@gmail.com">
-          <i class="bi bi-envelope-at-fill"></i>
+          <i class="bi bi-envelope-at-fill social-icon"></i>
         </a>
         <a
           href="/downloads/ZachRoblesSoftwareEng.pdf"
           download="ZachRoblesResume.pdf"
         >
-          <i class="bi bi-file-earmark-person-fill"></i>
+          <i class="bi bi-file-earmark-person-fill social-icon"></i>
         </a>
       </div>
     </div>
