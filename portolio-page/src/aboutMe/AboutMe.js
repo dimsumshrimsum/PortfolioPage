00c11565816a89ed39dcafefc09ee04a88cd7a0c
Zach Robles <zrobles1188@gmail.com>
2023-08-31 12:50:10 -0400

    spelling correction in about me section

--- portolio-page/src/aboutMe/AboutMe.js
+++ portolio-page/src/aboutMe/AboutMe.js
@@ -5,23 +5,23 @@ function AboutMe() {
         <h1>About Me</h1>
         <div className="col-md-8 d-flex align-items-center">
           <p>
-            Im Zachary, but please feel free to call me Zach. I'm inspired by
+            I'm Zachary, but please feel free to call me Zach. I'm inspired by
             learning new things and coding provides a limitless spring of new
             things to learn. Solving problems has always provided a unique
             degree of satisfaction, and the world of software development has
             given a level of tangibility to complex problems that has incited in
-            me, a passion for coding. Ive worked in customer service, ive worked
-            in sales, Ive driven forklifts and box trucks, Ive built patios and
-            delivered Appliances, all the while, one thing has been the same,
-            Ive loved being part of a team, Ive loved reaching towards a common
-            goal, and I've loved pushing myself. I may be new to the world of
-            sofware development, but I am eager and excited to learn and grow as
-            a developer. When Im not coding I train Brazilian Jiu Jitsu and Rock
-            Climb. I play a number of musical instruments, am an avid painter,
-            and I also enjoy table top games. I try to serve my community and
-            currently tutor local underserved spanish speaking 3rd-6th graders.
-            I strive for a greater good in all that I do and I hope to bring
-            that to the table as a developer.
+            me, a passion for coding. I've worked in customer service, i've
+            worked in sales, I've driven forklifts and box trucks, I've built
+            patios and delivered Appliances, all the while, one thing has been
+            the same, I've loved being part of a team, I've loved reaching
+            towards a common goal, and I've loved pushing myself. I may be new
+            to the world of software development, but I am eager and excited to
+            learn and grow as a developer. When I'm not coding I train Brazilian
+            Jiu Jitsu and Rock Climb. I play a number of musical instruments, am
+            an avid painter, and I also enjoy tabletop games. I try to serve my
+            community and I currently tutor local underserved spanish speaking
+            3rd-6th graders. I strive for a greater good in all that I do and I
+            hope to bring that to the table as a developer.
           </p>
         </div>
         <div className="col-md-4 d-flex align-items-center">
