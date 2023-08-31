8bdd69dda686e221417605974d9fe2d9558bf297
Zach Robles <zrobles1188@gmail.com>
2023-08-31 16:50:29 -0400

    social tags functioning and positioned properly need to format color because they are all blue right now, pdf is hosted on site

--- portolio-page/src/socials/Socials.js
+++ portolio-page/src/socials/Socials.js
@@ -4,10 +4,21 @@ function Socials() {
   return (
     <div className="socials-container">
       <div className="socials">
-        <i class="bi bi-linkedin"></i>
-        <i class="bi bi-github"></i>
-        <i class="bi bi-envelope-at-fill"></i>
-        <i class="bi bi-file-earmark-person-fill"></i>
+        <a href="https://www.linkedin.com/in/zacharyrobles/">
+          <i class="bi bi-linkedin"></i>
+        </a>
+        <a href="https://github.com/dimsumshrimsum">
+          <i class="bi bi-github"></i>
+        </a>
+        <a href="mailto:zrobles1188@gmail.com">
+          <i class="bi bi-envelope-at-fill"></i>
+        </a>
+        <a
+          href="/downloads/ZachRoblesSoftwareEng.pdf"
+          download="ZachRoblesResume.pdf"
+        >
+          <i class="bi bi-file-earmark-person-fill"></i>
+        </a>
       </div>
     </div>
   );
