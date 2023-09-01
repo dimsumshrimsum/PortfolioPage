b4c57346642a79721016c67768cf0870f1dcc06c
Zach Robles <zrobles1188@gmail.com>
2023-09-01 12:36:06 -0400

    finallyyy got the social buttons working and not looking terrible need to format littel further and decide if needs color but functioning, will likely need to clean up

--- portolio-page/src/socials/Socials.js
+++ portolio-page/src/socials/Socials.js
@@ -8,24 +8,31 @@ function Socials() {
           href="https://www.linkedin.com/in/zacharyrobles/"
           target="_blank"
           rel="noreferrer"
+          className="social-icon-linkedIn"
         >
-          <i class="bi bi-linkedin social-icon"></i>
+          <span className="text-icon">linkedin</span>
+          <i className="bi bi-linkedin "></i>
         </a>
         <a
           href="https://github.com/dimsumshrimsum"
           target="_blank"
           rel="noreferrer"
+          className="social-icon-github"
         >
-          <i class="bi bi-github social-icon"></i>
+          <span className="text-icon">github</span>
+          <i className="bi bi-github"></i>
         </a>
-        <a href="mailto:zrobles1188@gmail.com">
-          <i class="bi bi-envelope-at-fill social-icon"></i>
+        <a href="mailto:zrobles1188@gmail.com" className="social-icon-email">
+          <span className="text-icon">email</span>
+          <i className="bi bi-envelope-at-fill"></i>
         </a>
         <a
           href="/downloads/ZachRoblesSoftwareEngineer.pdf"
           download="ZachRoblesResume.pdf"
+          className="social-icon-resume"
         >
-          <i class="bi bi-file-earmark-person-fill social-icon"></i>
+          <span className="text-icon">resume</span>
+          <i className="bi bi-file-earmark-person-fill"></i>
         </a>
       </div>
     </div>
