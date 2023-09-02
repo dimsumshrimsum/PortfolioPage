45919a3f8f1fbb2d8a9a27f8b0e76a9fe2dacad1
Zach Robles <zrobles1188@gmail.com>
2023-09-01 21:28:46 -0400

    further update to background image css to account for mobile brower viewport changes when scrolling

--- portolio-page/src/App.js
+++ portolio-page/src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
 import Home from "./landingElements/Home";
+import "./index.css";
 function App() {
   return (
     <div className="App">
+      <div id="backgroundWrapper"></div>
       <Home />
     </div>
   );
